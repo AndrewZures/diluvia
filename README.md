1127fdd57859ef41eb1f9e1adea126bd208e9fa5
Andrew Zures <azures@8thlight.com>
2014-09-26 13:46:31 -0500

    adds basic postgres connection

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Diluvia
 =======
 
 ** TODO: Add description **
+
+### Migrations
+mix ecto.gen.migration Elixir.Diluvia.DB.Repo <% migration_name %>
--- lib/diluvia/db/repo.ex
+++ lib/diluvia/db/repo.ex
@@ -1,12 +1,12 @@
-defmodule Diluvia.Repo do
+defmodule Diluvia.DB.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
-  # def conf do
-  #   parse_url "ecto://postgres:postgres@localhost/diluvia_dev"
-  # end
-  #
-  # def priv do
-  #   app_dir(:Diluvia, "priv/repo")
-  # end
+  def conf do
+    parse_url "ecto://postgres:postgres@localhost/diluvia"
+  end
+
+  def priv do
+    app_dir(:diluvia, "priv/repo")
+  end
 
 end
