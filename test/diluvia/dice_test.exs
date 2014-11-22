76727ca8c529850955afbd62dc75a49c54c94d0a
Andrew Zures <azures@8thlight.com>
2014-11-22 09:20:20 -0600

    test pass but wrong alg

--- test/diluvia/dice_test.exs
+++ test/diluvia/dice_test.exs
@@ -17,11 +17,11 @@ defmodule Diluvia.DiceTest do
     assert outcomes == [[1,2,3,4,5,6], [1,2,3,4,5,6,7,8]]
   end
 
-  # test "generates all possibilities for a d6" do
-  #   data = [{2,1},{3,2},{4,3},{5,4},{6,5},{7,6},{13,1},{11,2},{10,3},{9,4},{8,5}]
-  #   expected = Dice.flat_duplicate_many(data)
-  #   assert expected == Dice.all_possible_outcomes([6, 6])
-  # end
+  test "generates all possibilities for a d6" do
+    data = [{2,1},{3,2},{4,3},{5,4},{6,5},{7,6},{12,1},{11,2},{10,3},{9,4},{8,5}]
+    expected = Enum.sort(Dice.flat_duplicate_many(data))
+    assert expected == Dice.blah([6, 6, 6])
+  end
 
   test "replicates many" do
     data = [{2, 2}, {4, 3}]
@@ -32,11 +32,5 @@ defmodule Diluvia.DiceTest do
     data = [{2, 2}, {4, 3}]
     assert Dice.flat_duplicate_many(data) == [2,2,4,4,4]
   end
-
-  test "generates stuff" do
-    data = [{1, 2}, {2, 3}]
-    assert Dice.blah(data) == [{[1,1],[2,2,2]}]
-  end
-
 
 end
