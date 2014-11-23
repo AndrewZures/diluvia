228654f6b4003b61e37fb4e3ee72f66dbcab3011
Andrew Zures <azures@8thlight.com>
2014-11-23 17:06:08 -0600

    refactors tests

--- test/diluvia/util/dice/random_dice_test.exs
+++ test/diluvia/util/dice/random_dice_test.exs
@@ -13,37 +13,37 @@ defmodule Diluvia.Test.RandomDiceTest do
   test "rolls a random number for a single d4" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([4]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [1,2,3,4]
+    assert uniq_rolls == Enum.to_list(1..4)
   end
 
   test "rolls a random number for a single d6" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [1,2,3,4,5,6]
+    assert uniq_rolls == Enum.to_list(1..6)
   end
 
   test "rolls a random number for a single d8" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([8]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [1,2,3,4,5,6,7,8]
+    assert uniq_rolls == Enum.to_list(1..8)
   end
 
   test "rolls a random number for two d4" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([4,4]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [2,3,4,5,6,7,8]
+    assert uniq_rolls == Enum.to_list(2..8)
   end
 
   test "rolls a random number for two d6" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6,6]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [2,3,4,5,6,7,8,9,10,11,12]
+    assert uniq_rolls == Enum.to_list(2..12)
   end
 
   test "rolls a random number for two d6 and d4" do
     uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6,6,4]) end)
                  |> Enum.uniq |> Enum.sort
-    assert uniq_rolls == [3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    assert uniq_rolls == Enum.to_list(3..16)
   end
 
 end
