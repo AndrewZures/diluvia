37c1d9ee685c82dc48898f2c6e30cab81e132c0e
Andrew Zures <azures@8thlight.com>
2014-11-23 17:19:07 -0600

    refactors dice, uncomments json tests

--- test/diluvia/util/dice/random_dice_test.exs
+++ test/diluvia/util/dice/random_dice_test.exs
@@ -5,45 +5,44 @@ defmodule Diluvia.Test.RandomDiceTest do
   @moduletag :random
 
   test "rolls a random number" do
-    uniq_rolls = Enum.map(1..100, fn(_) -> Dice.roll([2]) end)
-                 |> Enum.uniq |> Enum.sort
+    uniq_rolls = uniq_outcomes([2])
     assert uniq_rolls == [1,2]
   end
 
-  test "rolls a random number for a single d4" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([4]) end)
-                 |> Enum.uniq |> Enum.sort
+  test "rolls a random number for single d4" do
+    uniq_rolls = uniq_outcomes([4])
     assert uniq_rolls == Enum.to_list(1..4)
   end
 
-  test "rolls a random number for a single d6" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6]) end)
-                 |> Enum.uniq |> Enum.sort
+  test "rolls a random number for single d6" do
+    uniq_rolls = uniq_outcomes([6])
     assert uniq_rolls == Enum.to_list(1..6)
   end
 
-  test "rolls a random number for a single d8" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([8]) end)
-                 |> Enum.uniq |> Enum.sort
+  test "rolls a random number for single d8" do
+    uniq_rolls = uniq_outcomes([8])
     assert uniq_rolls == Enum.to_list(1..8)
   end
 
   test "rolls a random number for two d4" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([4,4]) end)
-                 |> Enum.uniq |> Enum.sort
+    uniq_rolls = uniq_outcomes([4,4])
     assert uniq_rolls == Enum.to_list(2..8)
   end
 
   test "rolls a random number for two d6" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6,6]) end)
-                 |> Enum.uniq |> Enum.sort
+    uniq_rolls = uniq_outcomes([6,6])
     assert uniq_rolls == Enum.to_list(2..12)
   end
 
   test "rolls a random number for two d6 and d4" do
-    uniq_rolls = Enum.map(1..200, fn(_) -> Dice.roll([6,6,4]) end)
-                 |> Enum.uniq |> Enum.sort
+    uniq_rolls = uniq_outcomes([6,6,4], 300)
     assert uniq_rolls == Enum.to_list(3..16)
+  end
+
+  def uniq_outcomes(dice, n \\ 200) do
+    Enum.map(1..n, fn(_) -> Dice.roll(dice) end)
+    |> Enum.uniq
+    |> Enum.sort
   end
 
 end
